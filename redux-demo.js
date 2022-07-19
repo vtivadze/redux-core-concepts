2c25a71bd482d0b31de2eea9ddbc4e8fd9e17be0
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-19 12:36:08 +0400

    Create counter subscriber

--- redux-demo.js
+++ redux-demo.js
@@ -7,3 +7,8 @@ const counterReducer = (state, action) => {
 };
 
 const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
