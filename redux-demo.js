a3089d220f5e791a27fc67ec261edb521c145949
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-19 12:32:52 +0400

    Create counter reducer

--- redux-demo.js
+++ redux-demo.js
@@ -1,5 +1,9 @@
 const redux = require('redux');
 
-const counterReducer = () => {};
+const counterReducer = (state, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
 
-const store = redux.createStore();
+const store = redux.createStore(counterReducer);
