812d1b437239f5498faf478acc591395d16d5f89
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-19 12:42:54 +0400

    Subscribe on state changing

--- redux-demo.js
+++ redux-demo.js
@@ -1,14 +1,20 @@
 const redux = require('redux');
 
+// reducer udates state (pure function)
 const counterReducer = (state, action) => {
   return {
     counter: state.counter + 1,
   };
 };
 
+// create state store which will be updated by 'counterReducer'
 const store = redux.createStore(counterReducer);
 
+// react on changing of state
 const counterSubscriber = () => {
   const latestState = store.getState();
   console.log(latestState);
 };
+
+// subscribe on updating of state
+store.subscribe(counterSubscriber);
